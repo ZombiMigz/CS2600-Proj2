3f68459ac04dcc596212a35d5c52b915e22e7cd5
Luciano Lim <lmlim@cpp.edu>
2022-03-05 21:42:06 -0800

    implements travelFees function

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+#include "tripTime.h"
 
 int main()
 {
+  // start program
+  printf("Welcome to our Trip Expenses Calculator.\n*************************\nPlease respond to the following questions.\n");
+
+  // trip time functions
+  days = getDaysOnTrip();
+  startTime = getStartTime();
+  endTime = getEndTime();
   return 0;
 }
