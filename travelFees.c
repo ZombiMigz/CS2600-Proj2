16c6c8603b9ebcf99aeccd138d6e2ca3f1a71865
Isaiah <iazelayandia@cpp.edu>
2022-03-07 15:49:22 -0800

    initialize travelFees file

---
+++ travelFees.c
@@ -0,0 +1,15 @@
+// various static travel fees
+#include <stdio.h>
+#ifndef TRAVELFEES_H
+#define TRAVELFEES_H
+#include "main.h"
+
+// asks the user for various costs
+
+//struct Expense calculateAirfare();
+
+//struct Expense calculateCarRentals();
+
+//struct Expense calculateGasCost();
+
+//struct Expense calculateConferenceOrSeminarFees();
