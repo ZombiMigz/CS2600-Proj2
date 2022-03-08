f8b4faae5ecb0048dd41b099bf914068f9a3953a
Luciano Lim <lmlim@cpp.edu>
2022-03-07 21:50:59 -0800

    defines functions in variableFees.c

--- variableFees.c
+++ variableFees.c
@@ -1,26 +1,43 @@
-int calculateParkingFees(int day, int pay)
+#include "main.h"
+#include "variableFees.h"
+#include <stdio.h>
+struct Expense calculateParkingFees(int *day)
 {
-//The company allows up to $6 per day. Anything more than this must be paid by 
-// the employee so we can reuse to calculate his spending easily
-    int total; 
-    total = day * pay;
-    return total;
+    // The company allows up to $6 per day. Anything more than this must be paid by
+    //  the employee so we can reuse to calculate his spending easily
 
+    printf("\n Enter the cost of parking on day %d: $", *day);
+    float cost;
+    scanf("%f", &cost);
+
+    struct Expense expense;
+    expense.expectedCost = 6;
+    expense.actualCost = cost;
+    return expense;
 }
 
-
-int calculateTaxiFees(int day , int pay)
+struct Expense calculateTaxiFees(int *day)
 {
     // The company allows up to $10 per day
-    int total; 
-    total = day * pay;
-    return total;
+    printf("\n Enter the cost of taxi fees on day %d: $", *day);
+    float cost;
+    scanf("%f", &cost);
+
+    struct Expense expense;
+    expense.expectedCost = 10;
+    expense.actualCost = cost;
+    return expense;
 }
 
-int calculateHotelExpenses(int day, int pay)
+struct Expense calculateHotelExpenses(int *day)
 {
-    //The company allows up to $90 per night for lodging. Anything more than this  must be paid by the employee
-    int total; 
-    total = day * pay;
-    return total;
+    // The company allows up to $90 per night for lodging. Anything more than this  must be paid by the employee
+    printf("\n Enter the cost of lodging on night %d: $", *day);
+    float cost;
+    scanf("%f", &cost);
+
+    struct Expense expense;
+    expense.expectedCost = 90;
+    expense.actualCost = cost;
+    return expense;
 }
