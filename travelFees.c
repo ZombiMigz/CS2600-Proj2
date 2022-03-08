fe9b86a5b6597e5d0f423870b140b307209474fc
Isaiah <iazelayandia@cpp.edu>
2022-03-07 16:14:38 -0800

    airfare complete

--- travelFees.c
+++ travelFees.c
@@ -6,7 +6,17 @@
 
 // asks the user for various costs
 
-//struct Expense calculateAirfare();
+struct Expense calculateAirfare()
+{
+    printf("\n Please provide the cost of roundtrip airfare:");
+    float fees;
+    scanf("%f", &fees); 
+
+    struct Expense AirFare; 
+    AirFare.expectedCost = 0; //idk what this should be
+    AirFare.actualCost = fees;
+    return AirFare;
+};
 
 //struct Expense calculateCarRentals();
 
