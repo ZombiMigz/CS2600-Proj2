7a46e774c816b87b1d44840466b4d652174a2044
Luciano Lim <lmlim@cpp.edu>
2022-03-07 20:27:49 -0800

    updates formatting in travelFees.c

--- travelFees.c
+++ travelFees.c
@@ -4,52 +4,50 @@
 
 // asks the user for various costs
 
-struct Expense calculateAirfare() 
+struct Expense calculateAirfare()
 {
-    printf("\n Please provide the cost of roundtrip airfare:");
+    printf("\n Please provide the cost of roundtrip airfare: $");
     float fees;
-    scanf("%f", &fees); 
+    scanf("%f", &fees);
 
-    struct Expense AirFare; 
-    AirFare.expectedCost = 0; //idk what this should be
+    struct Expense AirFare;
+    AirFare.expectedCost = 0; // idk what this should be
     AirFare.actualCost = fees;
     return AirFare;
 };
 
 struct Expense calculateCarRentals()
 {
-    printf("\n Please provide the amount for any and all car rentals");
+    printf("\n Please provide the amount for any and all car rentals: $");
     float rentals;
-    scanf("%f", &rentals); 
+    scanf("%f", &rentals);
 
-    struct Expense CarRental; 
-    CarRental.expectedCost = 0; //idk what this should be
+    struct Expense CarRental;
+    CarRental.expectedCost = 0; // idk what this should be
     CarRental.actualCost = rentals;
     return CarRental;
 };
 
 struct Expense calculateGasCost()
 {
-    printf("\n Please provide the miles traveled in a private vehicle");
+    printf("\n Please provide the miles traveled in a private vehicle: ");
     float miles;
     scanf("%f", &miles);
 
-    struct Expense GasCost; 
-    GasCost.expectedCost = 0;//idk what this should be
+    struct Expense GasCost;
+    GasCost.expectedCost = 0; // idk what this should be
     GasCost.actualCost = miles * .27;
     return GasCost;
 }; // ask for mile and multiply by $.27
 
-
 struct Expense calculateConferenceOrSeminarFees()
 {
-    printf("\n Please provide the miles traveled in a private vehicle");
-        float comSemFee;
-        scanf("%f", &comSemFee); 
+    printf("\n Please provide the cost of Conference of Seminar Fees: $");
+    float comSemFee;
+    scanf("%f", &comSemFee);
 
-
-        struct Expense ConferenceOrSeminar; 
-        ConferenceOrSeminar.expectedCost = 0;//idk what this should be
-        ConferenceOrSeminar.actualCost = comSemFee;
-        return ConferenceOrSeminar;
+    struct Expense ConferenceOrSeminar;
+    ConferenceOrSeminar.expectedCost = 0; // idk what this should be
+    ConferenceOrSeminar.actualCost = comSemFee;
+    return ConferenceOrSeminar;
 };
